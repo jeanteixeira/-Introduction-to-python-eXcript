44fde01362f69ea2cd9758b0db07f5b52d55a968
Jean Barros <jean.barros.teixeira@gmail.com>
2016-11-03 14:07:01 -0300

    Learning about decision making

---
+++ basic module/decision_making.py
@@ -0,0 +1,20 @@
+print("CONDICAO BASICA")
+
+a = 10
+
+if(a==10):
+	print("O valor de a e igual a 10")
+
+
+print
+
+print("CONDICAO 2")
+acao = int(input("Digite '1' para sim e digite '2' para nao" ))
+
+if(acao==1):
+	print("Voce disse que sim")
+else:
+	if(acao==2):
+		print("Voce disse que nao")
+	else:
+		print("o numero e diferente de '1' e '2'")
