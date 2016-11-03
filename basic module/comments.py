9f2cc985955c69e16cd78b6fa9ed08c4bd1c6ff6
Jean Barros <jean.barros.teixeira@gmail.com>
2016-11-03 10:35:27 -0300

    Learning comments, indentation and variables

---
+++ basic module/comments.py
@@ -0,0 +1,11 @@
+#print("primeira linha")
+print("segunda linha")#comentario sobre uma linha
+
+"""
+Comentario
+em
+um 
+bloco
+"""
+
+print("terceira linha")
---
+++ basic module/indentation_and_variables.py
@@ -0,0 +1,21 @@
+#indentacao
+if True:
+	print("verdadeiro")
+
+#variaveies e tipos de variaveis
+a = 10
+b = 2
+print(a+b)
+print(type(a))
+
+texto = "TEXTO"
+print(texto)
+print(type(texto))
+
+dinheiro = 15.55
+print(dinheiro)
+print(type(dinheiro))
+
+minha_variavel = "variavel"
+valor_total = 10
+
