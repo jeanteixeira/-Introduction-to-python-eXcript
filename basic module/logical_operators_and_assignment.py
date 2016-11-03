92cad1dbb8aea196a1b38401d4dd0a99e0d6c638
Jean Barros <jean.barros.teixeira@gmail.com>
2016-11-03 15:31:45 -0300

    Learning logical operators and assignment

---
+++ basic module/logical_operators_and_assignment.py
@@ -0,0 +1,26 @@
+print("OPERADORES LOGICOS")
+print(2<4 and 2==4)
+print(2>4 or 2<4)
+print(not(2>4 or 2<4))
+print(2 is 2)
+print('a' is 'a')
+print(2 is 3)
+print(type(2))
+print(type(2) is int)
+print(type(2.0) is int)
+print(type(2.0) is float)
+
+print
+
+print("OPERDORES DE ATRIBUICAO")
+print
+
+x,y = 2,4
+print(x)
+print(y)
+
+x+=y
+
+print(x)
+
+
