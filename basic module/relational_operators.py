451a46490404b75f5fb65d864dce00c4bd5dbe4b
Jean Barros <jean.barros.teixeira@gmail.com>
2016-11-03 14:21:17 -0300

    Add more examples

--- basic module/relational_operators.py
+++ basic module/relational_operators.py
@@ -5,3 +5,41 @@ print(5>3)
 print(1>=0.999999)
 
 print((5<5.1) or (5>=5.1))
+
+print
+print("OPERADORES RELACIONAIS COMPOSTOS")
+print
+
+idade = int(input("Informe a sua idade:"))
+if(idade<=0):
+	print("A sua idade nao pode ser 0 ou menor que 0!")
+elif(idade>150):
+	print("A sua idade nao pode ser superior a 150 anos!")
+elif(idade<18):
+	print("Voce precisa ter mais que 18 anos!")
+		
+print
+
+numero1 = int(input("Digite um numero"))
+numero2 = int(input("Digite o segundo numero"))
+
+if(numero1==numero2):
+	print("O numero %d e igual a %d" %(numero1, numero2))
+if(numero1!=numero2):
+	print("O numero %d e diferente de %d" %(numero1, numero2))
+if(numero1>numero2):
+	print("O numero %d e maior que %d" %(numero1, numero2))
+if(numero1<numero2):
+	print("O numero %d e menor que%d" %(numero1, numero2))
+if(numero1>=numero2):
+	print("O numero %d e maior ou igual a %d" %(numero1, numero2))
+if(numero1<=numero2):
+	print("O numero %d e menor ou igual a %d" %(numero1, numero2))	
+	
+	
+	
+	
+	
+	
+	
+	
