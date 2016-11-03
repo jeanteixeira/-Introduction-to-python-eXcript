5a8f2ca4d8a774407b66a1ef7c3dfda969395192
Jean Barros <jean.barros.teixeira@gmail.com>
2016-11-03 11:35:29 -0300

    learning about math operations

---
+++ basic module/math_operations.py
@@ -0,0 +1,39 @@
+#operacoes matematicas basicas
+print("OPERACOES BASICAS")
+print
+print(10-10)
+print(1000-80)
+print(10/5)
+print(10/6)
+print(10//6)
+
+print
+
+#modulo da divisao
+print("MODULO DA DIVISAO")
+print
+print(6%2)
+print(3%2)
+print(4%2)
+print(6%2==0)
+
+print
+
+num1 = float(input("Digite um numero"))
+num2  = float(input ("Digite outro numero"))
+print
+
+divisao = num1 / num2
+resto = num1 % num2
+
+print(num1,"dividido por", num2, "e igual a", divisao)
+print("O resto da divisao entre", num1, "e", num2, "e igual a", resto)
+
+print
+#potenciacao e radiciacao
+print("POTENCIACAO E RADICIACAO")
+print
+
+print(5**2)
+print(25**0.5)
+
