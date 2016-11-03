349782ed3e1bbe5cb26845163d32bf95dd648137
Jean Barros <jean.barros.teixeira@gmail.com>
2016-11-03 11:00:49 -0300

    learning about manipulating data

---
+++ basic module/manipulating_data.py
@@ -0,0 +1,15 @@
+num_int = 5
+num_dec = 7.3
+val_str = "Jean"
+
+print("O valor e:", num_int)
+print("O valor e:%i" %num_int)
+print("O valor e:" + str(num_int))
+
+print("concatenando decimal: ", num_dec)
+print("concatenando decimal: %.2f" %num_dec) 
+print("concatenando decimal: " + str(num_dec))
+
+print("concatenando strings", val_str)
+print("concatenando strings: %s" %val_str)
+print("concatenando strings" + val_str)
