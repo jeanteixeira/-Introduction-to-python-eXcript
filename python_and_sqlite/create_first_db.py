ae8c709ecd9db6ec902c89cb7ced65559fc5599d
Jean Barros <jean.barros.teixeira@gmail.com>
2016-11-04 16:48:44 -0300

    read db

--- python_and_sqlite/create_first_db.py
+++ python_and_sqlite/create_first_db.py
@@ -3,3 +3,9 @@ import sqlite3
 path = r'/home/jean/Introduction-to-python-eXcript/python_and_sqlite'
 conn = sqlite3.connect(path+r'/teste.db')
 
+cursor = conn.execute('select * from contatos')
+rows = cursor.fetchall()
+
+for row in rows:
+	print(row)
+
