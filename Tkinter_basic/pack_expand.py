3535b71e2de01c055c8437f047ca186d63a773f8
Jean Barros <jean.barros.teixeira@gmail.com>
2016-11-15 09:49:48 -0300

    .

---
+++ Tkinter_basic/pack_expand.py
@@ -0,0 +1,12 @@
+from Tkinter import *
+
+top = Frame() ; top.pack(fill='both', expand=True)
+a = Label (top, text="A") ; a.pack(side="left",fill="y")
+b = Label (top, text="B") ; b.pack(side="bottom",fill="x")
+c = Label (top, text="C") ; c.pack (side="right")
+d = Label (top, text="D") ; d.pack (side="top")
+
+for widget in (a,b,c,d):
+	widget.configure(relief="groove", border=10,font="Times 24 bold")
+
+top.mainloop()
